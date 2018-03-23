31762233e4cd10c0afbe26299f0ec00b339df2e7
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-03-23 09:10:21 -0700

    add initial file system

---
+++ src/api.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+
+
+
+export class Api {
+//api calls
+
+  //Movie DB API TAKES IN A MOVIE TITLE
+  // callMovieDBApi(title) {
+  //   return new Promise(function(resolve, reject){
+  //     const movieDBKey = process.env.MOVIE_KEY;
+  //     let request = new XMLHttpRequest();
+  //     let url = `https://api.themoviedb.org/3/search/movie?api_key=${movieDBKey}&query=${title}`;
+  //     request.onload = function() {
+  //       if (this.status === 200) {
+  //         //returns the response at fulfillment of promise
+  //         resolve(request.response);
+  //       } else {
+  //         reject(alert(request.statusText));
+  //       }
+  //     }
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  // }
+}
